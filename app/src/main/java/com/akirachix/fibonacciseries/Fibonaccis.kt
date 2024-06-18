85f7b820998f7c69c72b7144831edb638a0d0a7d
Mary-Vivian <muthonimaryvivian>
2024-06-18 18:31:06 +0300

    Fibonacci series

--- app/src/main/java/com/akirachix/fibonacciseries/Fibonaccis.kt
+++ app/src/main/java/com/akirachix/fibonacciseries/Fibonaccis.kt
@@ -1,14 +1,30 @@
 package com.akirachix.fibonacciseries
 
-class  Fibonaccis{
-    fun generatefibonacci(count:Int):List<Int>{
-        val fibonacciNumbers= mutableListOf<Int>()
-        if(count>0)fibonacciNumbers.add(0)
-        if(count>1)fibonacciNumbers.add(1)
-        for (i in 2 until count){
-            val nextNumber=fibonacciNumbers[i-1] +fibonacciNumbers[i-2]
-            fibonacciNumbers.add(nextNumber)
-        }
-        return fibonacciNumbers
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Fibonaccis (var numbers:List<Int>):RecyclerView.Adapter<NumbersViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val numberView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fibonacci_numbers,parent,false)
+        return NumbersViewHolder(numberView)
     }
+
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.txtresult.text = numbers[position].toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
+
 }
+
+class NumbersViewHolder(numberView:View):RecyclerView.ViewHolder(numberView){
+    val txtresult= numberView.findViewById<TextView>(R.id.txtresult)
+}
+
--- app/src/main/java/com/akirachix/fibonacciseries/MainActivity.kt
+++ app/src/main/java/com/akirachix/fibonacciseries/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.akirachix.fibonacciseries
 
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akirachix.fibonacciseries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+    lateinit var binding: ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding=ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            val numbers = fibonacci(100)
+            binding.tvResult.layoutManager = LinearLayoutManager(this)
+
+            val numberAdapter=Fibonaccis(numbers)
+            binding.tvResult.adapter=numberAdapter
+
+
+        }
+        fun fibonacci(n: Int): List<Int> {
+            val numbers = mutableListOf(0, 1)
+            while (numbers.size < n) {
+                numbers.add(numbers[numbers.lastIndex] + numbers[numbers.lastIndex - 1])
+            }
+            return numbers
+        }
+
 
 
     }
-}
+
